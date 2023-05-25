3dc6cdd8c25202b69eea1c016f20b4a64a5bf5d0
20120196 <20120196@student.hcmus.edu.vn>
2023-05-25 19:55:33 +0700

    fix meal summary context

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -3,15 +3,12 @@ import classes from "./MealsSummary.module.css";
 const MealsSummary = () => {
     return (
         <section className={classes.summary}>
-            <h2>Delicious Food, Delivered To You</h2>
+            <h2>Your trusted food delivery partner</h2>
             <p>
                 Choose your favorite meal from our broad selection of available
                 meals and enjoy a delicious lunch or dinner at home.
             </p>
-            <p>
-                All our meals are cooked with high-quality ingredients,
-                just-in-time and of course by experienced chefs!
-            </p>
+            <p>Amazing food delivery at your service</p>
         </section>
     );
 };
